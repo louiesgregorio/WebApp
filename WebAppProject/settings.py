086c1f45236b4b57b6441975a7192280b25d2d3f
louiesgregorio <107600540+louiesgregorio@users.noreply.github.com>
2022-06-16 21:35:07 +0800

    Update settings.py

--- WebAppProject/settings.py
+++ WebAppProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,11 +28,12 @@ SECRET_KEY = 'django-insecure-t&!xkshb^-aem790^$crw78g@_#5i-%zao@6s5gfpk7nd=fibr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+django_heroku.settings(locals())
 
-STATIC_URL = "CSUShop/static/"
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://csushop.herokuapp.com/']
+ALLOWED_HOSTS = ['https://csushop.herokuapp.com/']
 
 
 # Application definition
@@ -81,15 +83,24 @@ WSGI_APPLICATION = 'WebAppProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'df5ngvjdq836nf',
+        'USER' : 'vwgjzidzytqmey',
+        'PASSWORD' : 'd9e9b4e3c38ffea91930c2597d7f52f41fc06b4027f1a75218983fb0f9f4153b',
+        'HOST' : 'ec2-54-165-178-178.compute-1.amazonaws.com',
+        'PORT' : '5432',
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
